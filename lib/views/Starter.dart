96e4590d5fb6b39d0f0c7b5d221d9149f6f7c3fa
khuong2924 <truongkhuong666@gmail.com>
2025-04-24 23:39:20 +0700

    update starter page

---
+++ lib/views/Starter.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+class StarterPage extends StatefulWidget {
+  const StarterPage({Key? key}) : super(key: key);
+
+  @override
+  _StarterPageState createState() => _StarterPageState();
+}
+
+class _StarterPageState extends State<StarterPage> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  
+  final List<Map<String, String>> _onboardingData = [
+    {
+      'title': 'We Provide The Best Electronic\nProducts From Great Brands',
+      'description': 'You will be able to find a wide selection of electronics from top brands'
+    },
+    {
+      'title': 'Discover Latest Technology',
+      'description': 'Explore the newest gadgets and devices from leading manufacturers'
+    },
+    {
+      'title': 'Shop With Confidence',
+      'description': 'Enjoy secure shopping with warranty and customer support'
+    }
+  ];
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: BoxDecoration(
+          // Background pattern similar to the image
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage('assets/images/pattern_background.png'),
+            fit: BoxFit.cover,
+            opacity: 0.2,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              // Logo section
+              Expanded(
+                flex: 3,
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/logo.png',
+                        width: 60,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        'Leuze',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 42,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              
+              // Content section
+              Expanded(
+                flex: 4,
+                child: PageView.builder(
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _currentPage = index;
+                    });
+                  },
+                  itemCount: _onboardingData.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            _onboardingData[index]['title']!,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            _onboardingData[index]['description']!,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              
+              // Indicator dots
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  _onboardingData.length,
+                  (index) => Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentPage == index ? Colors.white : Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+              
+              const SizedBox(height: 40),
+              
+              // Next button
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: InkWell(
+                  onTap: () {
+                    if (_currentPage < _onboardingData.length - 1) {
+                      _pageController.nextPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeIn,
+                      );
+                    } else {
+                      // Navigate to home or login page
+                      Navigator.pushReplacementNamed(context, '/home');
+                    }
+                  },
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
